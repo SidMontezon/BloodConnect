d9e33066cf8999647afaedd5ed81422832c68987
Jayjay22 <jayjaysoriano723@gmail.com>
2025-10-10 12:14:01 +0800

    FIles

---
+++ add-sample-data.bat
@@ -0,0 +1,17 @@
+@echo off
+echo ========================================
+echo BloodConnect Sample Data Adder
+echo ========================================
+echo.
+
+echo Installing Firebase dependencies...
+npm install firebase
+
+echo.
+echo Adding sample data to Firebase Realtime Database...
+node add-sample-data-simple.js
+
+echo.
+echo Press any key to exit...
+pause > nul
+
